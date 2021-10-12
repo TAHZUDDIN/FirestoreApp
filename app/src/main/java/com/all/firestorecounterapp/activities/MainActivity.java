ef42d09b0a7988e083fd0ef36c6a1e41d173548e
TAHZUDDIN AHMED <atahzuddin@yahoo.com>
2021-10-12 16:53:20 +0530

    Progress Alert  dialog..

--- app/src/main/java/com/all/firestorecounterapp/activities/MainActivity.java
+++ app/src/main/java/com/all/firestorecounterapp/activities/MainActivity.java
@@ -4,9 +4,13 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -34,6 +38,8 @@ public class MainActivity extends AppCompatActivity implements UpdateAdapter {
     SharedPreferences.Editor spe;
     UpdateAdapter updateInterface;
     private DatabaseReference db;
+    AlertDialog dialog;
+    ConstraintLayout emptyView;
 
 
     @Override
@@ -46,6 +52,8 @@ public class MainActivity extends AppCompatActivity implements UpdateAdapter {
         recyclerView.setLayoutManager(llm);
         recyclerView.setItemAnimator(null);
 
+        emptyView = findViewById(R.id.emptyView);
+
         sp = this.getPreferences(this.MODE_PRIVATE);
         spe = sp.edit();
         updateInterface = this;
@@ -54,13 +62,20 @@ public class MainActivity extends AppCompatActivity implements UpdateAdapter {
 
         counterList = new ArrayList<>();
 
+        showProgressDialog();
+
         db.get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
             @Override
             public void onComplete(@NonNull Task<DataSnapshot> task) {
+
+                hideProgressDialog();
+
                 if (!task.isSuccessful()) {
                     Log.e("firebase", "Error getting data", task.getException());
+                    showEmptyView();
                 }
                 else {
+                    hideEmptyView();
                     DataSnapshot ds = task.getResult();
                     counterList.clear();
 
@@ -69,9 +84,12 @@ public class MainActivity extends AppCompatActivity implements UpdateAdapter {
                         String key = d.getKey();
                         CounterModel cm = d.getValue(CounterModel.class);
                         cm.setDocumentId(key);
-                        int idOfString = getResources().getIdentifier(key,"string",getPackageName());
+                        int idOfString = getResources().getIdentifier(key+"_desc","string",getPackageName());
                         String desc = getString(idOfString);
                         cm.setDesc(desc);
+                        int idOfName = getResources().getIdentifier(key+"_name","string",getPackageName());
+                        String name = getString(idOfName);
+                        cm.setName(name);
                         counterList.add(cm);
 
                     }
@@ -81,6 +99,7 @@ public class MainActivity extends AppCompatActivity implements UpdateAdapter {
                     recyclerView.setAdapter(adapter);
                 }
             }
+
         });
 
     }
@@ -112,6 +131,32 @@ public class MainActivity extends AppCompatActivity implements UpdateAdapter {
         Intent intent = new Intent(this, DetailsActivity.class);
         intent.putExtra("model",model);
         startActivity(intent);
+    }
+
+
+    public void showProgressDialog(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        View v = LayoutInflater.from(this).inflate(R.layout.progress_layout,null,false);
+        builder.setView(v);
+        dialog = builder.create();
+        dialog.show();
+    }
+
+
+    public void hideProgressDialog(){
+        Log.i(TAG,"hideProgressDialog() called");
+        dialog.dismiss();
+    }
+
+
+    public void showEmptyView(){
+        emptyView.setVisibility(View.VISIBLE);
+        recyclerView.setVisibility(View.GONE);
+    }
+
+    public void hideEmptyView(){
+        emptyView.setVisibility(View.GONE);
+        recyclerView.setVisibility(View.VISIBLE);
     }
 
 }
